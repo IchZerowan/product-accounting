c6fa26617043d32faf0ca87a47dabed39a1b3b87
IchZerowan <Ih01@i.ua>
2021-10-16 12:47:09 +0300

    Backend Spring Boot project created

---
+++ product-accounting-backend/src/main/java/com/ichzerowan/accounting/ProductAccountingApplication.java
@@ -0,0 +1,13 @@
+package com.ichzerowan.accounting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductAccountingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductAccountingApplication.class, args);
+	}
+
+}
---
+++ product-accounting-backend/src/test/java/com/ichzerowan/accounting/ProductAccountingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ichzerowan.accounting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductAccountingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
