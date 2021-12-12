5cab3114545663d92acbcdb66e563f34aeec3b93
JNANEU <dlsunjan@gmail.com>
2021-12-13 00:36:26 +0300

    Added Product class and test method for GET request and Product object parsing.

--- product-accounting-frontend/product-accounting-frontend/MainWindow.xaml.cs
+++ product-accounting-frontend/product-accounting-frontend/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace product_accounting_frontend
 {
@@ -23,6 +26,33 @@ namespace product_accounting_frontend
         public MainWindow()
         {
             InitializeComponent();
+            startupCheckProducts();
+        }
+
+        private void startupCheckProducts()
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(@"http://product-accounting.us-east-2.elasticbeanstalk.com/products");
+            List<Product> products = new List<Product>();
+            string responseJSON;
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                responseJSON = reader.ReadToEnd();
+            }
+            textBlock1.Text = "";
+            textBlock1.Text = responseJSON;
+            try
+            {
+                products = JsonConvert.DeserializeObject<List<Product>>(responseJSON);
+            } catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            for(int i = 0; i < products.Count; i++)
+            {
+                Console.WriteLine(products[i].ToString());
+            }
         }
     }
 }
